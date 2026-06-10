e1b52a4195626969ee7d88eece8091d762a188cb
Terra Jentsch <terra.jentsch@domain.hernehill.com>
2026-06-10 14:15:16 -0500

    cmake outdated version changed.

--- share/cmake/macros/ParseArguments.cmake
+++ share/cmake/macros/ParseArguments.cmake
@@ -26,7 +26,7 @@
 # Downloaded from: http://www.itk.org/Wiki/CMakeMacroParseArguments
 #
 
-cmake_minimum_required(VERSION 2.4.7)
+cmake_minimum_required(VERSION 3.15)
 
 MACRO(PARSE_ARGUMENTS prefix arg_names option_names)
     SET(DEFAULT_ARGS)
