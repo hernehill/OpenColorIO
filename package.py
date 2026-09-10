ef9f58f56f3dd98a3e1834131390155fb49d82d5
Terra Jentsch <terra.jentsch@domain.hernehill.com>
2026-09-10 08:53:25 -0500

    Rebuild for Imath 3.1.9

--- package.py
+++ package.py
@@ -22,7 +22,7 @@
 name = "ocio"
 # name = "ocio_tools"
 
-version = "2.3.2.hh.1.0.1"
+version = "2.3.2.hh.1.0.2"
 
 authors = [
     "Sony Pictures Imageworks & AcademySoftwareFoundation",
@@ -44,7 +44,6 @@ def requires():
             # "libexpat",
             "pybind11",
             "imath",
-            "visual_studio",
         ]
     elif this.name == "ocio_tools":
         return [
@@ -54,13 +53,12 @@ def requires():
             "imath",
             "lcms",
             "oiio",
-            "visual_studio",
         ]
     else:
         raise ValueError("Wrong package name")
 
 
-private_build_requires = []
+private_build_requires = ["visual_studio"]
 
 variants = [
     ["python-3.9"],
